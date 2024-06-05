05784defd9005ff60f24c0cd8951292670a21665
iFHA <fernando.everywhere@gmail.com>
2024-06-05 19:20:13 -0300

    primeiro commit

---
+++ src/main/java/dev/fernando/spring_pro_ch01/SpringProCh01Application.java
@@ -0,0 +1,13 @@
+package dev.fernando.spring_pro_ch01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProCh01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProCh01Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fernando/spring_pro_ch01/SpringProCh01ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fernando.spring_pro_ch01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProCh01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
